b3aa5d7892d73e0d9af1eb18c85e2aa967274f14
whfweb <40481696+whfweb@users.noreply.github.com>
2025-12-05 13:38:02 +0800

    Update diy-part1.sh
    Signed-off-by: whfweb <40481696+whfweb@users.noreply.github.com>

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,10 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+cd package
+git clone https://github.com/rufengsuixing/luci-app-zerotier.git || true
+cd ..
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
